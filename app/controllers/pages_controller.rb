8abd3d4cd272b2bdb11f6c3c036d160297564a5f
Owen Moss <owenkmoss@gmail.com>
2019-01-21 12:02:59 +0100

    adding routes, controller actions and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,32 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def eat
+
+  end
+
+  def drink
+
+  end
+
+  def experience
+
+  end
+
+  def shop
+
+  end
+
+  def privatize
+
+  end
+
+  def gallery
+
+  end
+
+  def booking
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+
+  get 'eat', to: 'pages#eat', as: 'eat'
+  get 'drink', to: 'pages#drink', as: 'drink'
+  get 'experience', to: 'pages#experience', as: 'experience'
+  get 'shop', to: 'pages#shop', as: 'shop'
+  get 'privatize', to: 'pages#privatize', as: 'privatize'
+  get 'gallery', to: 'pages#gallery', as: 'gallery'
+  get 'booking', to: 'pages#booking', as: 'booking'
+
 end
