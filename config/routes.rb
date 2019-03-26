0657270757782cdf429ffa5f10f39fe9e51ebf36
Owen Moss <owenkmoss@gmail.com>
2019-03-26 13:47:38 +0100

    setup i18n for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
 
-  get 'location', to: 'pages#location', as: 'location'
-  get 'food_and_wines', to: 'pages#food_and_wines', as: 'food_and_wines'
-  get 'about_us', to: 'pages#about_us', as: 'about_us'
-  get 'menu', to: 'pages#menu', as: 'menu'
-  get 'contact', to: 'pages#contact', as: 'contact'
+  scope '(:locale)', locale: /en|es/ do
+    root to: 'pages#home'
+    get 'location', to: 'pages#location', as: 'location'
+    get 'food_and_wines', to: 'pages#food_and_wines', as: 'food_and_wines'
+    get 'about_us', to: 'pages#about_us', as: 'about_us'
+    get 'menu', to: 'pages#menu', as: 'menu'
+    get 'contact', to: 'pages#contact', as: 'contact'
+  end
 
 end
