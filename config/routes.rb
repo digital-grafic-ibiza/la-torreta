1026cdf1737d61cd77dcb5e82a95b009aee353bf
Owen Moss <owenkmoss@gmail.com>
2022-08-24 12:13:53 +0200

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope '(:locale)', locale: /en|es/ do
-    root to: 'pages#home'
-    get 'location', to: 'pages#location', as: 'location'
-    get 'food_and_wines', to: 'pages#food_and_wines', as: 'food_and_wines'
-    get 'about_us', to: 'pages#about_us', as: 'about_us'
-    get 'menu', to: 'pages#menu', as: 'menu'
-    get 'reserve', to: 'pages#reserve', as: 'reserve'
-  end
-
+      root to: 'pages#home'
+      get 'location', to: 'pages#location', as: 'location'
+      get 'food_and_wines', to: 'pages#food_and_wines', as: 'food_and_wines'
+      get 'about_us', to: 'pages#about_us', as: 'about_us'
+      get 'menu', to: 'pages#menu', as: 'menu'
+      get 'reserve', to: 'pages#reserve', as: 'reserve'
+    end
 end
